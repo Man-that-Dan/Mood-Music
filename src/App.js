30f6d64394a301f0f51f39f370217b12bed1d411
Daniel <dtoro@g.clemson.edu>
2019-05-07 23:17:36 -0400

    Can now view and sort playlist tracks. Added player

--- src/App.js
+++ src/App.js
@@ -14,8 +14,19 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      accessToken: null,
       loggedIn: false,
+      deviceId: "",
+      error: "",
+      trackName: "Track Name",
+      artistName: "Artist Name",
+      albumName: "Album Name",
+      playing: false,
+      position: 0,
+      duration: 0,
+      playerLaunched: null,
     };
+    this.playerCheckInterval = null;
   }
 
 
@@ -26,23 +37,153 @@ class App extends Component {
     } // Update the Redux state with new accessToken
   }
 
+  checkForPlayer() {
+  const { accessToken } = this.props;
+
+  if (window.Spotify !== null) {
+    clearInterval(this.playerCheckInterval);
+    this.player = new window.Spotify.Player({
+      name: "Mood-Music",
+      getOAuthToken: cb => { cb(accessToken); },
+    });
+    this.createEventHandlers();
+    this.setState({playerLaunched: true});
+    // finally, connect!
+    this.player.connect();
+  }
+}
+
+createEventHandlers() {
+  this.player.on('initialization_error', e => { console.error(e); });
+  this.player.on('authentication_error', e => {
+    console.error(e);
+    this.setState({ loggedIn: false });
+  });
+  this.player.on('account_error', e => { console.error(e); });
+  this.player.on('playback_error', e => { console.error(e); });
+
+  // Playback status updates
+  this.player.on('player_state_changed', state => this.onStateChanged(state));
+
+  // Ready
+  this.player.on('ready', async data => {
+    let { device_id } = data;
+    console.log("Let the music play on!");
+    await this.setState({ deviceId: device_id });
+    this.transferPlaybackHere();
+  });
+}
+
+handleLogin() {
+  if (this.props.accessToken !== null) {
+    this.setState({ loggedIn: true });
+    this.setState({ accessToken: this.props.accessToken });
+    // check every second for the player.
+    this.playerCheckInterval = setInterval(() => this.checkForPlayer(), 1000);
+  }
+}
+
+onStateChanged(state) {
+  // if we're no longer listening to music, we'll get a null state.
+  if (state !== null) {
+    const {
+      current_track: currentTrack,
+      position,
+      duration,
+    } = state.track_window;
+    const trackName = currentTrack.name;
+    const albumName = currentTrack.album.name;
+    const artistName = currentTrack.artists
+      .map(artist => artist.name)
+      .join(", ");
+    const playing = !state.paused;
+    this.setState({
+      position,
+      duration,
+      trackName,
+      albumName,
+      artistName,
+      playing
+    });
+  }
+}
+
+transferPlaybackHere() {
+  const { deviceId, token } = this.state;
+  fetch("https://api.spotify.com/v1/me/player", {
+    method: "PUT",
+    headers: {
+      authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      "device_ids": [ deviceId ],
+      "play": true,
+    }),
+  });
+}
+
+  // let redirect_uri = 'http://18.217.115.146:3000'
+  // let my_client_id = 'b73df62cecef48919ce8a24a6b520f42';
+  // let scopes = 'user-read-private user-read-email';
+  // let url = 'https://accounts.spotify.com/authorize' +
+  // '?response_type=code' +
+  // '&client_id=' + my_client_id +
+  // (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
+  // '&redirect_uri=' + encodeURIComponent(redirect_uri);
+  //
+
+  onPrevClick() {
+    this.player.previousTrack();
+  }
+
+  onPlayClick() {
+    this.player.togglePlay();
+  }
+
+  onNextClick() {
+    this.player.nextTrack();
+  }
+
+
+
 
   render() {
     return (
       <BrowserRouter>
         <div className="App">
           <header>
+            <div style={titleStyle}>
+            Mood-Music v1
+            </div>
             <nav>
               <ul>
-                <li> <Link to = "/"> Playlists </Link> </li>
+                <li> <Link style={TextStyle} to = "/"> Playlists </Link> </li>
+                <li> <Link style={TextStyle} to = "/"> About </Link> </li>
               </ul>
             </nav>
           </header>
-          {this.state.loggedIn === true ? null : <a href='https://spotisci-backend.herokuapp.com/login'> Login to Spotify </a> }
+            { this.state.accessToken === null ? <a href='http://18.217.115.146:3004/login'> Login to Spotify </a> : null }
+            { this.state.playerLaunched === null ? <button onClick={this.handleLogin.bind(this)} > then Launch player </button> : null }
+
 
           <Switch>
             { this.props.accessToken && <Route path="/" component={ Playlists }/> }
+
           </Switch>
+
+
+          { this.state.playerLaunched != null ? <div className="ControlBar">
+            <p>Artist: {this.stateartistName} Track: {this.statetrackName} Album: {this.statealbumName}</p>
+            <p>
+            <button onClick={() => this.onPrevClick()} >Previous</button>
+            <button onClick={() => this.onPlayClick()} >{this.state.playing ? "Pause" : "Play"}</button>
+            <button onClick={() => this.onNextClick()} >Next</button>
+            </p>
+
+
+
+            </div>: null}
 
         </div>
       </BrowserRouter>
@@ -50,6 +191,26 @@ class App extends Component {
   }
 }
 
+var TextStyle = {
+  backgroundColor : '#000000',
+  color: 'green',
+  padding: '6px',
+  borderRadius: '21%',
+  border: '3px solid green',
+  fontFamily: 'sans-serif'
+}
+
+var titleStyle = {
+  fontFamily: 'sans-serif',
+  fontWeight: 'bold',
+  textAlign: 'left',
+  padding: '1em',
+}
+
+var aboutStyle = {
+  padding: '2em',
+  fontFamily: 'sans-serif',
+}
 
 const mapStateToProps = state => {
   return {
@@ -58,7 +219,7 @@ const mapStateToProps = state => {
 }
 
 
-const mapDispathToProps = dispatch => {
+ const mapDispathToProps = dispatch => {
   return {
     onAccesTokenReceived: (accessToken) => dispatch({type: actionTypes.SET_ACCESS_TOKEN, accessToken: accessToken})
   }
--- src/components/FullPlaylist.js
+++ src/components/FullPlaylist.js
@@ -28,7 +28,8 @@ class FullPlaylist extends Component{
   state = {
     loadedPlaylistId: null,
     tracks: null,
-    features: null
+    features: null,
+    sortBy: "energy"
   }
 
   componentDidMount(){
@@ -44,11 +45,13 @@ class FullPlaylist extends Component{
           headers: {
             'Authorization' : 'Bearer ' + this.props.accessToken},
         }).then(response => response.json())
-        .then( data => { 
+        .then( data => {
           this.setState({
-            tracks: data.items.map( item => {return { 
+            tracks: data.items.map( item => {return {
               name: item.track.name,
-              id: item.track.id
+              artist: item.track.artists,
+              id: item.track.id,
+
            }}),
             loadedPlaylistId: data.href.split('/')[5]
           })
@@ -60,17 +63,46 @@ class FullPlaylist extends Component{
        }
     }
   }
-  
+
   fetchFeatures (tracks){
     fetch('https://api.spotify.com/v1/audio-features/?ids=' + tracks, {
       headers: {
         'Authorization' : 'Bearer ' + this.props.accessToken},
     }).then(response => response.json())
-    .then( data => { 
+    .then( data => {
       this.setState({
         features: data.audio_features
       })
+      var featuresArray = this.state.features;
+      var tracksArray = this.state.tracks;
+      for (var i = 0; i < featuresArray.length; i++) {
+        this.setState({
+          tracks: this.state.tracks.map(track => {
+
+            if (track.id === featuresArray[i].id) {
+              return {
+                ...track,
+                danceability: featuresArray[i].danceability,
+                acousticness: featuresArray[i].acousticness,
+                energy: featuresArray[i].energy,
+                speechiness: featuresArray[i].speechiness,
+                instrumentalness: featuresArray[i].instrumentalness,
+                liveness: featuresArray[i].liveness,
+                valence: featuresArray[i].valence,
+                key: featuresArray[i].key,
+                loudness: featuresArray[i].loudness,
+                mode: featuresArray[i].mode,
+                tempo: featuresArray[i].tempo
+              };
+            }
+            return track;
+
+          })
+
+        });
+    }
     })
+
   } // Make a request to get the features for all the tracks
 
   getAverage(features){
@@ -89,7 +121,7 @@ class FullPlaylist extends Component{
 
     for(let i = 0; i < features.length; i++){
       for(var feature in features[i]){
-        if(feature in sum[0]){      
+        if(feature in sum[0]){
           sum[0][feature] += parseFloat(features[i][feature])
         }
       }
@@ -102,12 +134,90 @@ class FullPlaylist extends Component{
     return sum[0]
   }
 
+findId(data, idToLookFor) {
+    var featuresArray = data.features;
+    for (var i = 0; i < featuresArray.length; i++) {
+        if (featuresArray[i].id == idToLookFor) {
+            return(featuresArray[i]);
+        }
+    }
+}
+
+setSort(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "danceability"
+  });
+}
+setSort2(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "energy"
+  });
+}
+setSort3(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "speechiness"
+  });
+}
+setSort4(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "accousticness"
+  });
+}
+setSort5(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "instrumentalness"
+  });
+}
+setSort6(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "liveness"
+  });
+}
+setSort7(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "valence"
+  });
+}
+setSort8(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "key"
+  });
+}
+setSort9(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "loudness"
+  });
+}
+setSort10(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "mode"
+  });
+}
+setSort11(){
+  console.log("clicked");
+  this.setState({
+    sortBy: "tempo"
+  });
+}
+
+
+
   render(){
-    let playlist = <p> Please select a playlist. </p> 
+    let playlist = <p> Please select a playlist. </p>
     let featuresQuant = null //Features that have 100% value, e.g. danceability
     let playlistFeatures = null
 
-    if(this.state.tracks && (this.state.loadedPlaylistId === this.props.id) && this.state.features ){  
+    if(this.state.tracks && (this.state.loadedPlaylistId === this.props.id) && this.state.features ){
       playlistFeatures = this.getAverage(this.state.features)
     }
     else{
@@ -120,22 +230,65 @@ class FullPlaylist extends Component{
       ))
     } //populate features
 
+    let trackslist = []
+    let metric = this.state.sortBy;
+
+    if (this.state.tracks) {
+      trackslist = this.state.tracks.sort( //Sort it ...
+        function(a,b) { // using a custom sort function that...
+          //  compares (the keys) by their respective values.
+
+            return b[metric] - a[metric];
+        }).map( track =>
+      <button style={trackstyle} className = 'Track' key = {track.id} >
+        {track.name}<br></br><p style={subfont}> {track.artist[0].name}<br></br>Danceability: {track.danceability} Energy: {track.energy} Speechiness: {track.speechiness}
+          Acousticness: {track.acousticness}
+          Instrumentalness: {track.Instrumentalness}
+          Liveness: {track.liveness}
+          Valence: {track.valence}
+          Key: {track.key}
+          Loudness: {track.loudness}
+          Mode: {track.mode}
+          Tempo: {track.tempo} </p>
+      </button>)//.sort( //Sort it ...
+      //  function(a,b) { // using a custom sort function that...
+            // compares (the keys) by their respective values.
+        //    return a - hash[b];
+        //})
+    }
+
     return(
       <div className = "FullPlaylist">
         <div className = "Playlist-container">
           { this.props.name ? <Playlist image = { this.props.image } name = {this.props.name}/> : null }
         </div>
         <div className = "Features">
-          <FeaturesNominal> 
+          <FeaturesNominal>
             { playlistFeatures && <Key value = { playlistFeatures.key } /> }
             { playlistFeatures && <Loudness value = { playlistFeatures.loudness } /> }
             { playlistFeatures && <Mode value = { playlistFeatures.mode } /> }
             { playlistFeatures && <Tempo value = { playlistFeatures.tempo } /> }
-          </FeaturesNominal> 
+          </FeaturesNominal>
           <FeaturesQuant>
-            { featuresQuant } 
+            { featuresQuant }
           </FeaturesQuant>
         </div>
+        <h3> Select Feature To Sort By </h3>
+        <div>
+
+          <button className="sortButton" onClick={this.setSort.bind(this)} > Danceability </button>
+          <button className="sortButton" onClick={this.setSort2.bind(this)} > Energy </button>
+          <button className="sortButton" onClick={this.setSort3.bind(this)} > Speechiness </button>
+          <button className="sortButton" onClick={this.setSort4.bind(this)} > Acousticness </button>
+          <button className="sortButton" onClick={this.setSort5.bind(this)} > Instrumentalness </button>
+          <button className="sortButton" onClick={this.setSort6.bind(this)} > Liveness </button>
+          <button className="sortButton" onClick={this.setSort7.bind(this)} > Valence </button>
+          <button className="sortButton" onClick={this.setSort8.bind(this)} > Key </button>
+          <button className="sortButton" onClick={this.setSort9.bind(this)} > Loudness </button>
+          <button className="sortButton" onClick={this.setSort10.bind(this)} > Mode </button>
+          <button className="sortButton" onClick={this.setSort11.bind(this)} > Tempo </button>
+        </div>
+        <div className = "Tracks"> { trackslist } </div>
       </div>
     )
   }
@@ -145,6 +298,20 @@ const mapStateToProps = state => {
   return {
     accessToken: state.accessToken
   };
+}
+
+var trackstyle = {
+  width: '100%',
+  backgroundColor: 'black',
+  border: '2px solid #ffffff',
+  color: 'white',
+  textAlign: 'left',
+  fontSize: '18px',
+}
+
+var subfont = {
+  color: '#444444',
+  fontSize: '1em'
 }
 
 export default connect(mapStateToProps) (FullPlaylist);
